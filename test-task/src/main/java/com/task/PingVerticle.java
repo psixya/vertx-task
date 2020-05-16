898517ff739852943a21f9d6d1ad794e55b74ae2
psixya13 <psixya13@gmail.com>
2020-05-16 13:03:39 +0300

    добавил брокера, должно работать

--- test-task/src/main/java/com/task/PingVerticle.java
+++ test-task/src/main/java/com/task/PingVerticle.java
@@ -1,8 +1,9 @@
 package com.task;
 
-import io.vertx.amqp.AmqpClient;
-import io.vertx.amqp.AmqpClientOptions;
-import io.vertx.amqp.AmqpConnection;
+import io.vertx.amqp.*;
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.http.HttpServerResponse;
 import org.vertx.java.platform.Verticle;
 
 
@@ -12,27 +13,63 @@ This is a simple Java verticle which receives `ping` messages on the event bus a
 public class PingVerticle extends Verticle {
 
   public void start() {
-
-
     AmqpClientOptions options = new AmqpClientOptions()
             .setHost("localhost")
             .setPort(5672)
             .setUsername("user")
             .setPassword("secret");
-    // Create a client using its own internal Vert.x instance.
-    AmqpClient client1 = AmqpClient.create(options);
+    AmqpClient client = AmqpClient.create(options);
 
-    // USe an explicit Vert.x instance.
-    AmqpClient client2 = AmqpClient.create(vertx, options);
-
-    client1.connect(ar -> {
+    client.connect(ar -> {
       if (ar.failed()) {
         System.out.println("Unable to connect to the broker");
       } else {
         System.out.println("Connection succeeded");
         AmqpConnection connection = ar.result();
+        connection.createAnonymousSender(responseSender -> {
+          // You got an anonymous sender, used to send the reply
+          // Now register the main receiver:
+          connection.createReceiver("my-queue", done -> {
+            if (done.failed()) {
+              System.out.println("Unable to create receiver");
+            } else {
+              AmqpReceiver receiver = done.result();
+              receiver.handler(msg -> {
+                // You got the message, let's reply.
+                responseSender.result().send(AmqpMessage.create()
+                        .address(msg.replyTo())
+                        .correlationId(msg.id()) // send the message id as correlation id
+                        .withBody("my response to your request")
+                        .build()
+                );
+              });
+            }
+          });
+        });
+
+        // On the sender side (sending the initial request and expecting a reply)
+        connection.createDynamicReceiver(replyReceiver -> {
+          // We got a receiver, the address is provided by the broker
+          String replyToAddress = replyReceiver.result().address();
+
+          // Attach the handler receiving the reply
+          replyReceiver.result().handler(msg -> {
+            System.out.println("Got the reply! " + msg.bodyAsString());
+          });
+
+          // Create a sender and send the message:
+          connection.createSender("my-queue", sender -> {
+            sender.result().send(AmqpMessage.create()
+                    .replyTo(replyToAddress)
+                    .id("my-message-id")
+                    .withBody("This is my request").build());
+          });
+        });
       }
     });
+
+
+
 
 //    vertx.eventBus().registerHandler("ping-address", new Handler<Message<String>>() {
 //      @Override
@@ -45,6 +82,5 @@ public class PingVerticle extends Verticle {
 //    container.logger().info("PingVerticle started");
   }
 
-  
 
 }
--- test-task/src/test/java/com/task/unit/ExampleUnitTest.java
+++ test-task/src/test/java/com/task/unit/ExampleUnitTest.java
@@ -26,6 +26,7 @@ public class ExampleUnitTest {
   public void testVerticle() {
     PingVerticle vert = new PingVerticle();
 
+
     // do something with verticle
   }
 }
